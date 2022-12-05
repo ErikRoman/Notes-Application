ea3940736b1a0c4bbf74a33c895ba23343202483
ErikRoman <118341316+ErikRoman@users.noreply.github.com>
2022-12-05 21:26:15 +0700

    start over

--- Source/filters.js
+++ Source/filters.js
@@ -1,26 +1,3 @@
 // Create filters object with searchText and sortBy property
 // Create getFilters function with no argument to return filters
 // Create setFilters function with updates object in argument to set updates for searchText and sortBy
-
-
-const filters = {
-  searchText: '',
-  sortBy: 'byEdited'
-}
-
-
-const getFilters = () => filters
-
-
-const setFilters = ( updates ) => {
-  if ( typeof updates.searchText === 'string' ) {
-    filters.searchText = updates.searchText
-  }
-
-  if ( typeof updates.sortBy === 'string' ) {
-    filters.sortBy = updates.sortBy
-  }
-}
-
-
-export { getFilters, setFilters }
